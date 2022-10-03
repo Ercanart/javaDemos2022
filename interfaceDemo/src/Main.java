7aeea06e6d25820e7a0d8a970fb45eae3e5114b3
Ercanart <ercan.artt@gmail.com>
2022-10-03 20:46:10 +0300

    firstCommit

---
+++ interfaceDemo/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) { //Veri tabanı uygulamalarına otomasyon denilir. Mesela bankacılık uygulamarıcd..
+        System.out.println("Hello world!");
+    }
+}
